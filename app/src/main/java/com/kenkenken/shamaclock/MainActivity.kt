773bb2002aa9d11e9243063b5038696188d2c052
yeokendra <kendrayeo18@gmail.com>
2022-03-16 02:34:32 +0700

    add twitter link

--- app/src/main/java/com/kenkenken/shamaclock/MainActivity.kt
+++ app/src/main/java/com/kenkenken/shamaclock/MainActivity.kt
@@ -2,10 +2,15 @@ package com.kenkenken.shamaclock
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.util.Linkify
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val tvTwitter: TextView = findViewById(R.id.tv_twitter);
+        Linkify.addLinks(tvTwitter, Linkify.ALL);
     }
 }
